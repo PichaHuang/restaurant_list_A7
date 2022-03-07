3da9acdf475a369e6db5d2ad51c33bc5c7931d8f
PichaHuang <ychuang33@gmail.com>
2022-03-07 21:36:31 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -19,16 +19,31 @@ db.once('open', () => {
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.static('public'))
 
 
 // 設定路由
-// 首頁
+// 首頁,瀏覽全部餐廳
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
 })
+// 瀏覽特定餐廳
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('detail', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+// 
+
+
+
+
 
 
 app.listen(3000, () => {
